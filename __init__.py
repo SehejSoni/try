b9cea1aa5e7ca3091065f8feb58a4ed69fbd58d6
SehejSoni <134284071+SehejSoni@users.noreply.github.com>
2024-02-28 02:06:38 -0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-@Time    : 2023/4/29 15:35
+@Time    : 2023/4/29 15:50
 @Author  : alexanderwu
 @File    : __init__.py
 """
 
-from enum import Enum
-from metagpt.tools import libs  # this registers all tools
-from metagpt.tools.tool_registry import TOOL_REGISTRY
-
-_ = libs, TOOL_REGISTRY  # Avoid pre-commit error
-
-
-class SearchEngineType(Enum):
-    SERPAPI_GOOGLE = "serpapi"
-    SERPER_GOOGLE = "serper"
-    DIRECT_GOOGLE = "google"
-    DUCK_DUCK_GO = "ddg"
-    CUSTOM_ENGINE = "custom"
+from metagpt.utils.read_document import read_docx
+from metagpt.utils.singleton import Singleton
+from metagpt.utils.token_counter import (
+    TOKEN_COSTS,
+    count_message_tokens,
+    count_string_tokens,
+)
 
 
-class WebBrowserEngineType(Enum):
-    PLAYWRIGHT = "playwright"
-    SELENIUM = "selenium"
-    CUSTOM = "custom"
-
-    @classmethod
-    def __missing__(cls, key):
-        """Default type conversion"""
-        return cls.CUSTOM
+__all__ = [
+    "read_docx",
+    "Singleton",
+    "TOKEN_COSTS",
+    "count_message_tokens",
+    "count_string_tokens",
+]
