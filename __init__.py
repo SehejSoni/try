7c25cd7a8ae45e65be9de16c72f2831eb0e4dd62
SehejSoni <134284071+SehejSoni@users.noreply.github.com>
2024-02-28 02:05:10 -0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,32 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Date    : 12/23/2023 4:51 PM
-# @Author  : stellahong (stellahong@fuzhi.ai)
-# @Desc    :
+"""
+@Time    : 2023/4/29 15:35
+@Author  : alexanderwu
+@File    : __init__.py
+"""
+
+from enum import Enum
+from metagpt.tools import libs  # this registers all tools
+from metagpt.tools.tool_registry import TOOL_REGISTRY
+
+_ = libs, TOOL_REGISTRY  # Avoid pre-commit error
+
+
+class SearchEngineType(Enum):
+    SERPAPI_GOOGLE = "serpapi"
+    SERPER_GOOGLE = "serper"
+    DIRECT_GOOGLE = "google"
+    DUCK_DUCK_GO = "ddg"
+    CUSTOM_ENGINE = "custom"
+
+
+class WebBrowserEngineType(Enum):
+    PLAYWRIGHT = "playwright"
+    SELENIUM = "selenium"
+    CUSTOM = "custom"
+
+    @classmethod
+    def __missing__(cls, key):
+        """Default type conversion"""
+        return cls.CUSTOM
